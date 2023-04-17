744e31e1b07ae72dd6b148c729906bbdae7b5d65
Steven Fearing <smfearing5@gmail.com>
2023-04-17 08:11:49 -0400

    Implement basic operation functions

---
+++ script.js
@@ -0,0 +1,34 @@
+// main
+
+
+// functions
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
+    switch(operator) {
+        case "+":
+            break;
+        case "-":
+            break;
+        case "*":
+            break;
+        case "/":
+            break;
+        default:
+            console.log("operate() switch ERROR");
+    };
+}
